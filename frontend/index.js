935e85ab87a47000e2fdd9ca8fa3e14ff3af4f36
LAVUDI MANASWI CHOWHAN <manaswilavudi@gmail.com>
2026-08-13 16:41:00 +0530

    new

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,27 @@
 // alert("Welcome to bootcamp");
+var users=[
+    {
+        "name":"John Doe",
+        "gender":"Male",
+        "image":"john.png"
+    },
+    {
+        
+        "name":"Jane Doe",
+        "gender":"Female",
+        "image":"jane.png"
+    }
+]
+
+var cur=0;
+function toggleuser(){
+    cur=(cur+1)%2;
+
+    var username=document.getElementById("user-name");
+    var usergender=document.getElementById("user-gender");
+    var userimg=document.getElementById("user-img");
+
+    username.innerHTML=users[cur].name;
+    usergender.innerHTML=users[cur].gender;
+    userimg.src=users[cur].image;
+}
