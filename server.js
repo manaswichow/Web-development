b231f8d60fcfa2daa7cc85a7187db5060c6b5b4f
LAVUDI MANASWI CHOWHAN <manaswilavudi@gmail.com>
2026-08-13 15:28:25 +0530

    a

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const app=express();
 const port =process.env.PORT||8080;
 app.use(express.static("frontend"));
 app.listen(port,function(){
-    console.log("Server running on http://localhost:"+port);
-});
+console.log("Server running on http://localhost:"+port);
+});  
