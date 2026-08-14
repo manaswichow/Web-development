91680a12fe5507151be64f45a73426ff5b3d6bd0
LAVUDI MANASWI CHOWHAN <manaswilavudi@gmail.com>
2026-08-14 10:46:29 +0530

    b

--- frontend/index.js
+++ frontend/index.js
@@ -23,5 +23,28 @@ function toggleuser(){
 
     username.innerHTML=users[cur].name;
     usergender.innerHTML=users[cur].gender;
-    userimg.src=users[cur].image;
+    userimg.src=users[cur].image;    
+}
+
+function randomuser(){
+    fetch("https://randomuser.me/api")
+    .then(function(res){
+        return res.json();
+    })
+    .then(function(data){
+ var username=document.getElementById("user-name");
+    var usergender=document.getElementById("user-gender");
+    var userimg=document.getElementById("user-img");
+
+    var newusername=data.results[0].name.first+""+data.results[0].name.last;
+    var newusergender=data.results[0].gender;
+    var newuserimg=data.results[0].picture.large;
+
+    username.innerHTML=newusername;
+    usergender.innerHTML=newusergender;
+    userimg.src=newuserimg
+    })
+    .catch(function(err){
+        console.log("Error occured: "+err);
+    })
 }
